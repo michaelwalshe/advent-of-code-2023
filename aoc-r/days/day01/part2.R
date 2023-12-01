9093779f9ff4e24b1216063376454824cdf7ac01
Michael Walshe <michael.j.t.walshe@gmail.com>
2023-12-01 10:10:30 +0000

    Improvement to part 2

--- aoc-r/days/day01/part2.R
+++ aoc-r/days/day01/part2.R
@@ -7,57 +7,18 @@ library(stringr)
 library(purrr)
 
 compute <- function(s = "") {
-  numbers <- c("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten")
-  numbers_c <- as.character(1:9)
+  # Part 2 improvement, rather than sliding windows just replace all instances
+  # of a word number with word<digit>word. This keeps overlaps
+  numbers <- c("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+  replacement <- str_c(numbers, 1:9, numbers)
 
-  find_numbers <- function(x) {
-    # Switch from regex to two pointer as need overlapping
-    # matches, e.g. twone -> two, one
-    len_x <- str_length(x)
-    out <- character(len_x)
-    l <- 1
-    r <- 1
-    out_index <- 1
-    while (TRUE) {
-      if (l > len_x) {
-        break
-      }
-
-      chars <- str_sub(x, l, r)
-
-      if (chars %in% numbers || chars %in% numbers_c) {
-        out[out_index] <- chars
-        out_index <- out_index + 1
-        l <- l + 1
-        r <- l
-      } else {
-        r <- r + 1
-      }
-
-      # Can have max 5 chars in word number, so that
-      # is max size of window
-      if (r - l >= 5 || r > len_x) {
-        l <- l + 1
-        r <- l
-      }
-    }
-    return(out[out != ""])
+  for (i in seq_along(numbers)) {
+    s <- str_replace_all(s, numbers[i], replacement[i])
   }
-
 
   s |>
     str_split_1("\n") |>
-    map(find_numbers) |>
-    map(\(x) {
-      # If have a digit, return as-is, otherwise find index
-      # of the word in our numbers vector
-      n <- suppressWarnings(as.numeric(x))
-      ifelse(
-        !is.na(n),
-        n,
-        match(x, numbers)
-      )
-    }) |>
+    str_extract_all("\\d") |>
     map_dbl(\(x) as.numeric(paste0(x[1], x[length(x)]))) |>
     sum()
 }
