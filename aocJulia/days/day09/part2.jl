f9929005e36bc25a5a2dd3e9c626369260a80660
Michael Walshe <michael.j.t.walshe@gmail.com>
2023-12-13 22:59:19 +0000

    Day 9 in julia

---
+++ aocJulia/days/day09/part2.jl
@@ -0,0 +1,39 @@
+using Chain
+using Pipe
+using Test
+
+using aocJulia: helpers
+
+
+function compute(s)
+    seqs = @chain s begin
+        chomp
+        split("\n")
+        split.(Ref(" "))
+        map(x -> parse.(Int, x), _)
+    end
+
+    tot = 0
+    for seq in seqs
+        add = 1
+        while any(seq .!= 0)
+            tot += add * seq[1]
+            add *= -1
+            seq = diff(seq)
+        end
+    end
+
+    return tot
+end
+
+
+INPUT_S = """0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45"""
+EXPECTED = 2
+
+@test compute(INPUT_S) == EXPECTED
+
+INPUT_TXT = helpers.read_input(dirname(Base.source_path()))
+
+@time println(compute(INPUT_TXT))
